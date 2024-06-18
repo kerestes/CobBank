16c5bec75b20c8be6b93131ce8996bfdee842544
Alexandre Kerestes <alexandrekerestes@gmail.com>
2024-06-18 10:20:48 +0200

    start spring backend for angular front-end, it makes the communication between cobol and front-end, middleware

---
+++ spring_backend/cobbank/src/main/java/fr/kerestes/cobbank/CobbankApplication.java
@@ -0,0 +1,13 @@
+package fr.kerestes.cobbank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CobbankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CobbankApplication.class, args);
+	}
+
+}
---
+++ spring_backend/cobbank/src/test/java/fr/kerestes/cobbank/CobbankApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.kerestes.cobbank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CobbankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
